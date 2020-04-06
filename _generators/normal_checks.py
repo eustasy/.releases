e3aed21a3050c7fea3587df49399ffba5ee81659
Lewis Goddard <lewisgoddard@eustasy.org>
2020-04-06 16:57:49 +0100

    Update normal_checks.py

--- _generators/normal_checks.py
+++ _generators/normal_checks.py
@@ -10,19 +10,23 @@ class Repo:
   def __init__(self, repo):
     self.repo = repo
 
-    travis = repo.get_contents(".travis.yml").content
+    try:
+      travis = repo.get_contents(".travis.yml").content
     
-    if "strb92da74ddf4b05b698e2d12ebd56e965d6749397" in travis:
-      self.normal_checks = '1.10.1'
-    elif "2b23ee3dbb274409ae51a620ae9d6fef6516781a" in travis:
-      self.normal_checks = '1.10.0'
-    elif "649a7e0907c0ab4b342688e7d068b574a0945b3e" in travis:
-      self.normal_checks = '1.9'
-    elif "4256f55ef631900df06ca5c6167e21e6ed4cf55b" in travis:
-      self.normal_checks = '1.7'
-    elif "d5f1a5d9e3fbac391b905f2bdfcdcdbfe465eabf" in travis:
-      self.normal_checks = '1.4'
-    else:
+      if "strb92da74ddf4b05b698e2d12ebd56e965d6749397" in travis:
+        self.normal_checks = '1.10.1'
+      elif "2b23ee3dbb274409ae51a620ae9d6fef6516781a" in travis:
+        self.normal_checks = '1.10.0'
+      elif "649a7e0907c0ab4b342688e7d068b574a0945b3e" in travis:
+        self.normal_checks = '1.9'
+      elif "4256f55ef631900df06ca5c6167e21e6ed4cf55b" in travis:
+        self.normal_checks = '1.7'
+      elif "d5f1a5d9e3fbac391b905f2bdfcdcdbfe465eabf" in travis:
+        self.normal_checks = '1.4'
+      else:
+        self.normal_checks = False
+    
+    except UnknownObjectException:
       self.normal_checks = False
 
 org = g.get_organization('eustasy')
