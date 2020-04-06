728fae7535504eff7ddec86b95f46641e0c753d8
Lewis Goddard <lewisgoddard@eustasy.org>
2020-04-06 18:46:39 +0100

    Update normal_checks.py

--- _generators/normal_checks.py
+++ _generators/normal_checks.py
@@ -11,19 +11,20 @@ class Repo:
     self.repo = repo
 
     try:
-      travis = repo.get_contents(".travis.yml").content
+      travis = repo.get_contents('.travis.yml').content
       print(travis)
       travis = base64.b64decode(travis)
+      print(travis)
     
-      if "strb92da74ddf4b05b698e2d12ebd56e965d6749397" in travis:
+      if 'strb92da74ddf4b05b698e2d12ebd56e965d6749397' in travis:
         self.normal_checks = '1.10.1'
-      elif "2b23ee3dbb274409ae51a620ae9d6fef6516781a" in travis:
+      elif '2b23ee3dbb274409ae51a620ae9d6fef6516781a' in travis:
         self.normal_checks = '1.10.0'
-      elif "649a7e0907c0ab4b342688e7d068b574a0945b3e" in travis:
+      elif '649a7e0907c0ab4b342688e7d068b574a0945b3e' in travis:
         self.normal_checks = '1.9'
-      elif "4256f55ef631900df06ca5c6167e21e6ed4cf55b" in travis:
+      elif '4256f55ef631900df06ca5c6167e21e6ed4cf55b' in travis:
         self.normal_checks = '1.7'
-      elif "d5f1a5d9e3fbac391b905f2bdfcdcdbfe465eabf" in travis:
+      elif 'd5f1a5d9e3fbac391b905f2bdfcdcdbfe465eabf' in travis:
         self.normal_checks = '1.4'
       else:
         self.normal_checks = False
