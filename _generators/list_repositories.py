c1c0d79a57f20363f1a80cc51c28376507e823a2
Lewis Goddard <lewis.goddard@eustasy.org>
2022-08-05 06:37:40 +0100

    Update list_repositories.py

--- _generators/list_repositories.py
+++ _generators/list_repositories.py
@@ -11,13 +11,20 @@ repos = []
 for repo in org.get_repos():
   # Skip archived repositories
   if repo.archived is False:
+    print (repo)
     print ('Processing {}...'.format(repo.name))
     repos.append(repo.name)
 
 #repos = sorted(repos, key=lambda repo: repo.name, reverse=False)
 
 with open('_data/list_repositories.yml', 'w') as file:
+    print ('Saving as YML')
     yaml.dump(repos, file)
   
 with open('_data/list_repositories.json', 'w') as file:
+<<<<<<< Updated upstream
     json.dump(repos, file, indent=2)
+=======
+    print ('Saving as JSON')
+    json.dump(repos, file, indent=2)
+>>>>>>> Stashed changes
