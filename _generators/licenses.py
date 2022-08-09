ed2a01b19c15a09abd4c602dabfb673d5f665a52
Lewis Goddard <lewis.goddard@eustasy.org>
2022-08-09 13:30:18 +0100

    Update licenses.py

--- _generators/licenses.py
+++ _generators/licenses.py
@@ -23,14 +23,18 @@ class Repo:
       license = base64.b64decode(license).decode("utf-8")
       #print(license)
       
-      if 'MIT' in license:
+      if 'MIT License' in license:
         self.license_type = 'MIT'
-      elif 'Apache' in license:
-        self.license_type = 'Apache'
-      elif 'GNU Lesser General Public License v3.0' in license:
-        self.license_type = 'GPLv3'
+      elif 'Apache License' and 'Version 2.0, January 2004' in license:
+        self.license_type = 'Apache 2.0'
+      elif 'GNU AFFERO GENERAL PUBLIC LICENSE' and 'Version 3, 29 June 2007' in license:
+        self.license_type = 'AGPLv3'
+      elif 'GNU Lesser General Public License v3.0' and 'Version 3, 29 June 2007' in license:
+        self.license_type = 'LGPLv3'
       elif 'GNU GENERAL PUBLIC LICENSE' and 'Version 3, 29 June 2007' in license:
-        self.license_type = '1.9'
+        self.license_type = 'GPLv3'
+      elif 'Mozilla Public License Version 2.0' in license:
+        self.license_type = 'Mozilla 2.0'
         
       print(self.license_type)
 
