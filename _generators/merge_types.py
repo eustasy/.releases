97bbfa71b1f83944b2723885be09177ad103fc20
Lewis Goddard <lewis.goddard@eustasy.org>
2022-08-10 13:27:40 +0100

    Update merge_types.py

--- _generators/merge_types.py
+++ _generators/merge_types.py
@@ -17,10 +17,9 @@ class NoReleasesFound(Exception):
 class Repo:
   def __init__(self, repo):
     self.name = repo.name
-    print(repo.allow_rebase_merge)
-    self.allow_rebase_merge = repo.allow_rebase_merge
-    self.allow_squash_merge = repo.allow_squash_merge
-    self.allow_merge_commit = repo.allow_merge_commit
+    self.allow_rebase_merge = True if repo.allow_rebase_merge is True else False
+    self.allow_squash_merge = True if repo.allow_squash_merge is True else False
+    self.allow_merge_commit = True if repo.allow_merge_commit is True else False
 
 org = g.get_organization('eustasy')
 
