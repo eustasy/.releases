de188e8417194722cb1e1544f38c0136955b6899
Lewis Goddard <lewis.goddard@eustasy.org>
2022-08-10 13:15:13 +0100

    Create merge_types.py

---
+++ _generators/merge_types.py
@@ -0,0 +1,51 @@
+import datetime
+import timeago
+import json
+import yaml
+import os
+import sys
+from github import Github
+from operator import itemgetter
+from github.GithubException import UnknownObjectException as GithubUnknownObjectException
+
+now = datetime.datetime.now()
+g = Github(os.environ['GITHUB_TOKEN'], per_page=100)
+
+class NoReleasesFound(Exception):
+  pass
+
+class Repo:
+  def __init__(self, repo):
+    self.name = repo.name
+    self.allow_rebase_merge = repo.allow_rebase_merge
+    self.allow_squash_merge = repo.allow_squash_merge
+    self.allow_merge_commit = repo.allow_merge_commit
+
+org = g.get_organization('eustasy')
+
+repos = []
+for repo in org.get_repos():
+  # Skip archived repositories
+  if repo.archived is False:
+    print ('Processing {}...'.format(repo.name))
+    try:
+      repos.append(Repo(repo))
+
+output = []
+for repo in repos:
+  output.append ({
+    "name": repo.name,
+    "allow_rebase_merge": repo.allow_rebase_merge,
+    "allow_squash_merge": repo.allow_squash_merge,
+    "allow_merge_commit": repo.allow_merge_commit
+  })
+
+output = sorted(output, key=itemgetter('name')) 
+
+with open('_data/unreleased_commits.yml', 'w') as file:
+    print ('Saving as YML')
+    yaml.dump(output, file)
+  
+with open('_data/unreleased_commits.json', 'w') as file:
+    print ('Saving as JSON')
+    json.dump(output, file, indent=2)
