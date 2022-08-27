785e680ef1646b006cfd7628a79d0dc1354a62a4
Lewis Goddard <lewis.goddard@eustasy.org>
2022-08-27 20:14:30 +0100

    Update normal_checks.py

--- _generators/normal_checks.py
+++ _generators/normal_checks.py
@@ -14,33 +14,13 @@ class Repo:
     self.normal_checks = False
 
     try:
-    
       normal = repo.get_contents('.github/workflows/normal.yml').content
       print(normal)
       normal = base64.b64decode(normal).decode("utf-8")
-      
       if 'af240a6c8960177bcb1d07815732df7eb15970c1' in normal:
         self.normal_checks = '3.0.1'
       elif '5408f4ab384ceebf276686578963498f1ade3f55' in normal:
         self.normal_checks = '3.0'
-        
-      travis = repo.get_contents('.travis.yml').content
-      print(travis)
-      travis = base64.b64decode(travis).decode("utf-8")
-      
-      if '143baa3fcdbd30302def591074cd44b09d42c4a4' in travis:
-        self.normal_checks = '2.0'
-      elif 'b92da74ddf4b05b698e2d12ebd56e965d6749397' in travis:
-        self.normal_checks = '1.10.1'
-      elif '2b23ee3dbb274409ae51a620ae9d6fef6516781a' in travis:
-        self.normal_checks = '1.10.0'
-      elif '649a7e0907c0ab4b342688e7d068b574a0945b3e' in travis:
-        self.normal_checks = '1.9'
-      elif '4256f55ef631900df06ca5c6167e21e6ed4cf55b' in travis:
-        self.normal_checks = '1.7'
-      elif 'd5f1a5d9e3fbac391b905f2bdfcdcdbfe465eabf' in travis:
-        self.normal_checks = '1.4'
-    
     except:
       pass
 
